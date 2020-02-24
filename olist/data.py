b551eed4dbf433d5c3814f20ee77a7df7c23d3f4
GabriellePrat <gabrielleprat@gmail.com>
2020-02-24 15:12:45 +0100

    added get_matching

--- olist/data.py
+++ olist/data.py
@@ -17,21 +17,39 @@ class Olist:
             data_dict[file[:-4]] = pd.read_csv('{}/data/csv/{}'.format(abs_path, file))
         return data_dict
 
-if __name__ == '__main__':
-    olist = Olist()
-    data_dict = olist.get_data()
-    print(data_dict.keys())
-    print(os.path.abspath(os.path.dirname(__file__)))
-
     def get_matching_table(self):
         """
         01-01 > This function returns a matching table between
         columns [`customer_id`, `customer_unique_id`,
         `order_id`, `seller_id`]
         """
+        list_col = ['customer_id', 'customer_unique_id', \
+            'order_id', 'product_id', 'seller_id']
+        data_dict = self.get_data()
+        orders_delivered_df = data_dict['olist_orders_dataset']\
+            [data_dict['olist_orders_dataset'].order_status == 'delivered']
+        merged_df = orders_delivered_df.merge(right=data_dict['olist_customers_dataset'],
+                          how='left',
+                          on="customer_id"
+                         )
+        merged_df = merged_df.merge(right=data_dict['olist_order_items_dataset'],
+                          how='left',
+                          on="order_id")
+        merged_df = data_dict['olist_products_dataset'].merge(right=merged_df,
+                          how='left',
+                          on="product_id")
+        merged_df = data_dict['olist_sellers_dataset'].merge(right=merged_df,
+                          how='left',
+                          on="seller_id")
+        final_df = merged_df[list_col]
+        return final_df
 
     def ping(self):
         """
         You call ping I print pong.
         """
         print('pong')
+
+if __name__ == '__main__':
+    olist = Olist()
+    print(olist.get_matching_table())
