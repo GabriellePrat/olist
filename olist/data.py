8794b126077c010721ba1f25b1880b2c7f529453
GabriellePrat <gabrielleprat@gmail.com>
2020-02-24 14:24:33 +0100

    DAY1 in progress

--- olist/data.py
+++ olist/data.py
@@ -11,6 +11,17 @@ class Olist:
         """
         # Hint: You will need to find the absolute path of the csv folder in order to call this method from anywhere.
         # Hint 2: look at python __file__ attribute
+        data_dict = {}
+        abs_path = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+        for file in os.listdir('{}/data/csv'.format(abs_path)):
+            data_dict[file[:-4]] = pd.read_csv('{}/data/csv/{}'.format(abs_path, file))
+        return data_dict
+
+if __name__ == '__main__':
+    olist = Olist()
+    data_dict = olist.get_data()
+    print(data_dict.keys())
+    print(os.path.abspath(os.path.dirname(__file__)))
 
     def get_matching_table(self):
         """
